68bc5fdf4aee4e42e5e1f9a05e2fd0445e714382
Mikhail Sidarkevich <42434528+chivekrodis@users.noreply.github.com>
2021-05-26 15:59:34 +0300

    EPMRPP-62268 - Provide basic reporting. examples (#2)
    * EPMRPP-62268 - provide basic reporting. examples
    
    * EPMRPP-62268 - code review fixes - 1
    
    * EPMRPP-62268 - code review fixes - 2
    
    Co-authored-by: Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>

---
+++ examples-webdriverio/example-webdriverio-jasmine/tests/foo.spec.js
@@ -0,0 +1,33 @@
+describe('Suite 1', () => {
+  it('Test should be PASSED', () => {
+    browser.url('https://webdriver.io');
+    const title = browser.getTitle();
+    expect(title).toBe(
+      'WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO',
+    );
+  });
+  xit('Test should be SKIPPED', () => {
+    browser.url('https://webdriver.io');
+    const title = browser.getTitle();
+    expect(title).toBe(
+      'WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO',
+    );
+  });
+  it('Test should be FAILED', () => {
+    browser.url('https://webdriver.io');
+    const title = browser.getTitle();
+    expect(title).toBe(
+      'WebdriverIO ·  Next-gen browser and mobile automation test framework for Node.js | WebdriverIO',
+    );
+  });
+  describe('should be type:TEST', () => {
+    it('test should be FAILED', () => {
+      expect(true).toBe(false);
+    });
+  });
+});
+describe('Suite 2', () => {
+  it('Test should be FAILED', () => {
+    expect(true).toBe(false);
+  });
+});
---
+++ src/storage.ts
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2021 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+import { Suite, TestItem } from './models';
+
+export class Storage {
+  private suites: Suite[] = [];
+
+  private testItems: TestItem[] = [];
+
+  public addSuite(data: Suite): void {
+    this.suites.push(data);
+  }
+
+  public getCurrentSuite(): Suite {
+    return this.suites[this.suites.length - 1] || null;
+  }
+
+  public removeSuite(suiteId: string): void {
+    this.suites = this.suites.filter(({ id }) => suiteId !== id);
+  }
+
+  public addTest(data: TestItem): void {
+    this.testItems.push(data);
+  }
+
+  public getCurrentTest(): TestItem {
+    return this.testItems[this.testItems.length - 1] || null;
+  }
+
+  public removeTest(testId: string): void {
+    this.testItems = this.testItems.filter(({ id }) => testId !== id);
+  }
+}
