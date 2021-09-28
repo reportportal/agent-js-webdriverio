5aa9446bab7214b9a1eae549801baff7fb15717f
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2021-09-28 14:45:36 +0300

    EPMRPP-67056 - remove examples folder

--- examples-webdriverio/example-webdriverio-cucumber/tests/stepDefinitions/then.js
+++
@@ -1,6 +0,0 @@
-const { Then } = require('@cucumber/cucumber');
-const assert = require('assert');
-
-Then(/^I expect the title of the page "([^"]*)"$/, (title) => {
-  assert.strictEqual(title, 'Google_2');
-});
