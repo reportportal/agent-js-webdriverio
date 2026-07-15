2498a12135a5a702a98c0eadf59f6c1583dbbeac
maria-hambardzumian <164881199+maria-hambardzumian@users.noreply.github.com>
2026-07-15 11:54:21 +0400

    EPMRPP-110528 || Implement service to join several parallel launches in wdio agent (#78)
    * EPMRPP-110528 Implement service to join several parallel launches in wdio agent
    
    * EPMRPP-110528 || crf
    
    * EPMRPP-110528 || crf-2
    
    * EPMRPP-110528 || Applied CodeRabbit AI suggestions
    
    * EPMRPP-110528 || Core review fixes - n
    
    * EPMRPP-110528 || Sync with develop
    
    * EPMRPP-110528 || Fix vulnerabilities
    
    ---------
    
    Co-authored-by: maria-hambardzumian <maria_hambardzumyan@gmail.com>
    Co-authored-by: Ilya_Hancharyk <Ilya_Hancharyk@epam.com>

--- src/types/clientHelpers.d.ts
+++ src/types/clientHelpers.d.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2021 EPAM Systems
+ *  Copyright 2026 EPAM Systems
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,7 +15,9 @@
  *
  */
 
-export { Reporter } from './reporter';
-export { ReportingApi } from './reportingApi';
-export { ReportPortalService } from './service';
-export { Config } from './models';
+declare module '@reportportal/client-javascript/lib/helpers' {
+  const helpers: {
+    now(): number;
+  };
+  export default helpers;
+}
